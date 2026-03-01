dc1877da4cf0830404799ad9587d91fe3faf5d0d
wMageblood <enzo.luciano@outlook.es>
2026-02-28 22:29:28 -0600

    Initial Commit

--- src/components/ApplyForm/ApplyForm.tsx
+++ src/components/ApplyForm/ApplyForm.tsx
@@ -1,1 +1,9 @@
-const ApplyForm = () => <p>123</p>
+const ApplyForm = () => {
+  return (
+    <div className={`flex justify-center items-center bg-green-500 w-lg h-lg`}>
+      <p className={`flex justify-center items-center`}>
+        Yo
+      </p>
+    </div>
+  )
+}
