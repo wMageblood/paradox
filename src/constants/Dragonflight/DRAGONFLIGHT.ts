2efc78d01cbaf432e8a7c6b00b80de85f7a9fe2e
wMageblood <enzo.luciano@outlook.es>
2026-03-14 05:14:07 -0600

    working on some stuff

---
+++ src/constants/Dragonflight/DRAGONFLIGHT.ts
@@ -0,0 +1,38 @@
+import * as VOTI from "../../assets/raids/dragonflight-raids/VotI/import-voti-images"
+import * as ABERRUS from "../../assets/raids/dragonflight-raids/AtSC/import-aberrus-images"
+import * as AMIRDRASSIL from "../../assets/raids/dragonflight-raids/AtDH/import-amirdrassil-images"
+
+export const ALL_DRAGONFLIGHT_RAIDS = [
+  [
+    { raidName: "Vault of the Incarnates", boss: "red humanoid dragon", order: 1, image: VOTI.eranog },
+    { raidName: "Vault of the Incarnates", boss: "humanoid big rock", order: 3, image: VOTI.terros },
+    { raidName: "Vault of the Incarnates", boss: "elemental council", order: 2, image: VOTI.council },
+    { raidName: "Vault of the Incarnates", boss: "wind elemental", order: 4, image: VOTI.windelemental },
+    { raidName: "Vault of the Incarnates", boss: "blue ice spider", order: 5, image: VOTI.sennarth },
+    { raidName: "Vault of the Incarnates", boss: "all elements guy", order: 6, image: VOTI.kurog },
+    { raidName: "Vault of the Incarnates", boss: "mother of eggs", order: 7, image: VOTI.diurna },
+    { raidName: "Vault of the Incarnates", boss: "lugia", order: 8, image: VOTI.raszageth },
+  ],
+  [
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "big plated dragon", order: 1, image: ABERRUS.kazzara },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "red and purple elementals", order: 2, image: ABERRUS.amalgamation },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "trio mafioso", order: 3, image: ABERRUS.forgottenexperiments },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "dont let em push", order: 4, image: ABERRUS.zaqali },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "tank killer", order: 5, image: ABERRUS.rashok },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "its a trap", order: 6, image: ABERRUS.zskarn },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "lava hydra", order: 7, image: ABERRUS.magmorax },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "coffee shop killer", order: 8, image: ABERRUS.neltharion },
+    { raidName: "Aberrus, the Shadowed Crucible", boss: "wipeador X bolencia", order: 9, image: ABERRUS.sarkareth },
+  ],
+  [
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "red tree", order: 1, image: AMIRDRASSIL.gnarlroot },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "mad zaqali woman", order: 2, image: AMIRDRASSIL.igira },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "fire serpent", order: 3, image: AMIRDRASSIL.volcoross },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "fire centaur", order: 4, image: AMIRDRASSIL.larodar },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "cuack", order: 5, image: AMIRDRASSIL.dreamcouncil },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "line boss", order: 6, image: AMIRDRASSIL.nymue },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "soak one, dont soak next", order: 7, image: AMIRDRASSIL.smolderon },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "fire druid", order: 8, image: AMIRDRASSIL.tindral },
+    { raidName: "Amirdrassil, the Dream's Hope", boss: "big red bad dragon", order: 9, image: AMIRDRASSIL.fyrakk },
+  ]
+];
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import RaidRenderer from "./components/About/RaidRenderer.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <About />
-    <RaidRenderer expansionName="Dragonflight" raidName="Amirdrassil, the Dream's Hope" raidKey="ABERRUS_THE_SHADOWED_CRUCIBLE" />
-    <RaidRenderer expansionName="Midnight" raidName="The Voidspire" raidKey="THE_VOIDSPIRE" />
+    <RaidRenderer expansionName="Dragonflight" />
   </StrictMode>,
 );
