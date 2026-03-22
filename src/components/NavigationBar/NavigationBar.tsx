cfb2ab123e4b7112ebc138d49e226a6534a59f72
Ana Paulina Eng Goon Lozoya <pau@air-de-ana.lan>
2026-03-22 02:24:48 -0600

    added some styling to sidebar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,22 +1,22 @@
 const NavigationBar = () => {
-  return (
-    <div className="mt-10 flex items-center justify-center">
-      <nav className="*:font-500 inset-shadow-lg bg-gray-neutral-400 mx-10 flex justify-between rounded-sm py-4 *:px-7 *:hover:underline">
-        <a className="" href="/about">
-          About
-        </a>
-        <a className="" href="/apply">
-          Apply
-        </a>
-        <a className="" href="/live">
-          Live
-        </a>
-        <a className="" href="/contact">
-          Contact
-        </a>
-      </nav>
-    </div>
-  );
+ return (
+  <div className="mt-10 flex items-center justify-center">
+   <nav className="*:font-500 inset-shadow-lg bg-gray-neutral-400 mx-10 flex justify-between rounded-sm py-4 *:px-7 *:hover:underline">
+    <a className="" href="/about">
+     About
+    </a>
+    <a className="" href="/apply">
+     Apply
+    </a>
+    <a className="" href="/live">
+     Live
+    </a>
+    <a className="" href="/contact">
+     Contact
+    </a>
+   </nav>
+  </div>
+ );
 };
 
 export default NavigationBar;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,38 +1,46 @@
-import anima from "../../assets/logo.png"
+import anima from "../../assets/logo.png";
 
 const Sidebar = () => {
+ const sidebarItems = [
+  { name: "Home", href: "./" },
+  { name: "Looking for Group", href: "looking-for-group" },
+  { name: "Blue Tracker", href: "blue-tracker" },
+  { name: "Class Changes", href: "class-changes" },
+  { name: "PvE Guides", href: "pve-guides" },
+  { name: "Leaderboards", href: "leaderboards" },
+  { name: "Stats", href: "stats" },
+  { name: "Tier List", href: "tier-list" },
+  { name: "Tournaments", href: "tournaments" },
+  { name: "Community", href: "community" },
+ ];
 
-
-  const sidebarItems = [
-     {name: "Home", href: "./"},
-     {name: "Looking for Group", href: "looking-for-group"},
-     {name: "Blue Tracker", href: "blue-tracker"},
-     {name: "Class Changes", href: "class-changes"},
-     {name: "PvE Guides", href: "pve-guides"},
-     {name: "Leaderboards", href: "leaderboards"},
-     {name: "Stats", href: "stats"},
-     {name: "Tier List", href: "tier-list"},
-     {name: "Tournaments", href: "tournaments"},
-     {name: "Community", href: "community"},
-  ];
-
-
-  return (
-    <div className="fixed top-0 left-0 w-64 bg-red-500 h-full flex flex-col">
-      <div><img src={anima} /></div>
-      <div>
-        <ul>
-          {sidebarItems.map(({name, href}) => <li><a href={href}>{name}</a></li>)}
-        </ul>
-      </div>
-      <div> Support Me </div>
-      <div className="">
-        <div> DISCORD • X</div>
-        <div> © XXXXXXX • vX.XX</div>
-      </div>
-    </div>
-  );
+ return (
+  <div className="fixed top-0 left-0 flex h-full w-64 flex-col bg-red-500">
+   <div>
+    <img src={anima} />
+   </div>
+   <div className="*:bg-blue-500 flex *:px-5 *:py-2 my-5">
+    <button className="w-full ml-3 rounded-l-sm">PvE</button>
+    <button className="w-full mr-3 rounded-r-sm">PvP</button>
+   </div>
+   <div>
+    <ul>
+     {sidebarItems.map(({ name, href }) => (
+      <li className="mx-3 py-1 hover:bg-blue-500 hover:rounded-sm">
+       <a className="ml-2" href={href}>
+        {name}
+       </a>
+      </li>
+     ))}
+    </ul>
+   </div>
+   <div> Support Me </div>
+   <div className="">
+    <div> DISCORD • X</div>
+    <div> © XXXXXXX • vX.XX</div>
+   </div>
+  </div>
+ );
 };
-
 
 export default Sidebar;
