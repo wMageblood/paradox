fa3255e89b0d8b9b382d74c505aa52ee4747567f
wMageblood <enzo.luciano@outlook.es>
2026-03-29 18:47:31 -0600

    test

--- src/features/pve/components/PvE.tsx
+++ src/features/pve/components/PvE.tsx
@@ -8,7 +8,7 @@ const Pve = () => {
   return (
     <div>
       <img className="block mx-auto mt-45" width={375} src={mainLogo} />
-      <p className="text-center text-sm text-gray-600">Ato Ata, chiguegue guata, ele eau'te e auta'</p>
+      <p className="text-center text-sm text-gray-600">Ato Ata, chiguegue guata, ele eau'te e auta' eee ata oto</p>
       <div className="flex mt-15 items-center justify-center">
         <SearchBar placeholder="esh-galakrond" searchbarStyling="w-2xl bg-primary-900 rounded-l-xl focus:ring-0 focus:border-1 border-1 border-primary-900" buttonText="🔎 Search"/>
       </div>
