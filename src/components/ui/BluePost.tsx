667903410d2097849dfa8575dcff54d710cf84fe
Ana Paulina Eng Goon Lozoya <pau@air-de-ana.lan>
2026-03-30 05:07:29 -0600

    further work on bluepost

--- src/components/ui/BluePost.tsx
+++ src/components/ui/BluePost.tsx
@@ -9,17 +9,17 @@ type BluePostProps = Partial<{
 }>;
 
 
-const BluePost = ({image, title, tags, author, age}: BluePostProps) => {
+const BluePost = ({title, tags, author, age}: BluePostProps) => {
   return (
-    <div className="bg-green-500">
-      <div className="mt-5 flex p-3">
+    <div className="mr-2 rounded-sm">
+      <div className="flex p-4 hover:bg-blue-500/50">
         <img src={questionMark} className=" aspect-3/2 w-15 rounded-sm"/>
         <div>
-          <h1 className="font-500 ml-2">Class Tuning Incoming - March 31</h1>
+          <h1 className="font-500 ml-2">{title}</h1>
           <div className="ml-2 flex gap-3">
-            <p>PVP WARRIOR</p>
-            <p>author</p>
-            <p>age</p>
+            <a href="#">{tags}</a>
+            <a href="#">{author}</a>
+            <a href="#">{age}</a>
           </div>
         </div>
       </div>
--- src/features/blue-tracker/components/BlueTracker.tsx
+++ src/features/blue-tracker/components/BlueTracker.tsx
@@ -2,13 +2,16 @@ import BluePost from "../../../components/ui/BluePost";
 
 const BlueTracker = () => {
   return (
-    <div className="">
-      <div className="mt-15 flex justify-between items-center bg-red-500 mr-2">
+    <div className=" bg-amber-500 mr-2">
+      <div className="mt-15 flex justify-between items-center bg-red-500">
         <h1 className="text-2xl font-500"> Blue Tracker: </h1>
         <a className="text-sm text-primary-600">{`View All ->`}</a>
       </div>
-      <div>
-        <BluePost />
+      <div className="grid grid-cols-2 mt-5 bg-blue-500 p-2 rounded-sm overflow-hidden">
+        <BluePost tags="warrior mage druid" author="Kyveza" age="02/07/2026" title="The easiest tier since Emerald Nightmare"  />
+        <BluePost tags="warrior mage druid" author="Kyveza" age="02/07/2026" title="The easiest tier since Emerald Nightmare"  />
+        <BluePost tags="warrior mage druid" author="Kyveza" age="02/07/2026" title="The easiest tier since Emerald Nightmare"  />
+        <BluePost tags="warrior mage druid" author="Kyveza" age="02/07/2026" title="The easiest tier since Emerald Nightmare"  />
       </div>
     </div>
   );
